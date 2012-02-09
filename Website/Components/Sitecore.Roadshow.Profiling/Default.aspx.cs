3b6a20a85cbabfb2eb2f308b81ca94357eba3e35
Nadine Lendzian <nle@sitecore.net>
2012-02-09 16:11:38 +0100

    Profiling Projekt hinzugefügt

---
+++ Website/Components/Sitecore.Roadshow.Profiling/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Data.DataAccess;
+
+namespace Sitecore.Roadshow.Profiling
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Tracker.CurrentVisit != null)
+            {
+                if ( Tracker.CurrentVisit.Location != null)
+                    litUserLocation.Text = string.Format("Location= {0}", Tracker.CurrentVisit.Location.Country);
+            }
+            
+        }
+    }
+}
