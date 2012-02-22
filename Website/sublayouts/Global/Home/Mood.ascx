6d7c128be5b4ed091426cc359fc20f8ac74ea60b
Nadine Lendzian <nle@sitecore.net>
2012-02-22 13:18:02 +0100

    Content hinzugefügt Sublayout für Teaser Navigation erstellt

---
+++ Website/sublayouts/Global/Home/Mood.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Mood.ascx.cs" Inherits="Sitecore.Roadshow.sublayouts.Global.Home.Mood" %>
+
+  <div class="mood">  
+    <sc:Image Field="Mood Image" ID="moodimage" runat="server" MaxWidth="970"  />
+    
+  </div>
---
+++ Website/sublayouts/Global/Home/Mood.ascx.cs
@@ -0,0 +1,67 @@
+﻿namespace Sitecore.Roadshow.sublayouts.Global.Home
+{
+    using System;
+using Sitecore.Data.Items;
+using Sitecore.Web.UI.WebControls;
+using Sitecore.Web;
+using Sitecore.Collections;
+    using Sitecore.Base.Web.Controls;
+
+    public partial class Mood : SublayoutBase
+    {
+        //private Item _dataSourceItem;
+
+
+        private void Page_Load(object sender, EventArgs e)
+        {
+             moodimage.Item = DataSourceItem;
+        }
+    //    public Item DataSourceItem
+    //    {
+
+    //        get
+    //        {
+    //            if (this._dataSourceItem == null)
+    //            {
+    //                Sublayout parent = this.Parent as Sublayout;
+    //                if (parent == null)
+    //                {
+    //                    this._dataSourceItem = Sitecore.Context.Item;
+    //                }
+    //                else if (string.IsNullOrEmpty(parent.DataSource))
+    //                {
+    //                    this._dataSourceItem = Sitecore.Context.Item;
+    //                }
+    //                else
+    //                {
+    //                    string dataSource = parent.DataSource;
+    //                    this._dataSourceItem = Sitecore.Context.Database.GetItem(dataSource) ?? Sitecore.Context.ContentDatabase.GetItem(dataSource);
+    //                    if (this._dataSourceItem == null)
+    //                    {
+    //                        this._dataSourceItem = Sitecore.Context.Item;
+    //                    }
+    //                }
+    //            }
+    //            return this._dataSourceItem;
+    //        }
+    //    }
+
+    //           public string DataSourceItemPath
+    //{
+    //    get
+    //    {
+    //        return this.DataSourceItem.Paths.FullPath;
+    //    }
+    //}
+
+    //public SafeDictionary<string> Parameters
+    //{
+    //    get
+    //    {
+    //        return WebUtil.ParseQueryString(((Sublayout) this.Parent).Parameters);
+    //    }
+    //}
+
+    }
+
+}
